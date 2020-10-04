d062336665e9b78a22c547d56df7d2d5df762197
Mngz <mngz636@gmail.com>
2020-10-04 14:57:31 +0200

    Add files via upload

---
+++ header_2.php
@@ -0,0 +1,31 @@
+
+<div id=header class="header row" style="rgba(0,0,0,0.2);
+box-shadow: -1px -1px 7px 0px rgba(0,0,0,0.2);" >
+<div class="col-sm-3" >
+<div>
+<a href=https://www.productlists.co.za/index.php class="logo logo_start" ></a>
+<a href=https://www.productlists.co.za/index.php class="title logo" >Productlists</a>
+<a href="#" onclick="toggleUpperScroll();return false;" class="toggleUpperScroll highlight" ></a>
+<a href="#" onclick="toggleSignIn();return false;" class="toggleSignIn highlight" ></a>
+</div>
+</div>
+<div class="col-sm-6" >
+    
+<div class="upper_scroll row" >
+    <div class=col-sm-6 >
+       <input style="width:100%;height:30px;border:0px;padding:4px;" onfocus=s_in(); onblur=s_out(this); type=search onkeyup="search(this.value);" class=" shadow" id=search placeholder="search" />   
+    </div>
+  <div class=col-sm-6 style="padding-top:25px;font-size:1.2em;text-align:center;">
+<a class=space href=https://www.productlists.co.za/services/directory_listings/company_directory.php >Companies</a>
+<a class=space href=https://www.productlists.co.za/services/sell/products/products.php >Products</a>
+<a class=space href=https://www.productlists.co.za/services/build_your_brand/columns.php >Columns</a>
+  </div>
+</div>
+
+<?php include (str_replace('\\','/',$_SERVER['DOCUMENT_ROOT']).'/feature/sign/signin_body.php'); ?>
+
+</div>
+<div id=header_right class="col-sm-3">
+<?php include str_replace('\\','/',$_SERVER['DOCUMENT_ROOT']).'/feature/sign/signin.php'; ?>
+</div>
+</div>
---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+
+session_start();
+
+$conn = new mysqli('localhost','produc10_mng','mngzpass636','produc10_productlists');
+
+$LOCATION = 'index.php';
+
+?>
+<!DOCTYPE html>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Productlists</title>
+<link rel=stylesheet href=https://productlists.co.za/bootstrap.min.css />
+<link rel=stylesheet href=https://productlists.co.za/style.css />
+<link rel=stylesheet href=https://productlists.co.za/mobstyle.css />
+
+<link rel=stylesheet href=https://productlists.co.za/index_style.css />
+
+<link rel=stylesheet href=https://www.productlists.co.za/scroll_style.css />
+
+<link rel=stylesheet href=https://productlists.co.za/feature/sign/style.css />
+<link rel=stylesheet href=https://productlists.co.za/feature/search/style.css />
+
+<meta name="viewport" content="width=device-width" />
+
+<link rel="shortcut icon" type="image/png" href="logo.png" />
+<meta name="description" content="Technology Firm - working towards solutions to free the youth. PRODUCTLISTS" />
+<meta name="keywords" content="productlists,product,sell,discount,cheap,latest,best selling,local,mzansi,south africa,pietermaritzburg,kzn,online shopping,technology,share files,music,documents,content,learn,education,information" />
+<meta name="author" content="Mongezi Mafunda" />
+
+
+</head>
+<body>
+<script>
+<?php 
+
+if(ISSET($_SESSION['response'])){
+	echo 'alert("'.$_SESSION['response'].'");';
+	$_SESSION['response'] = null;
+}
+
+?>
+</script>
+<script src=https://productlists.co.za/api.js ></script>
+<script src=https://productlists.co.za/feature/search/api.js ></script>
+<script src=https://productlists.co.za/feature/sign/api.js ></script>
+<script src=https://productlists.co.za/services/sell/products/feature/option/api.js ></script>
+
+
+<?php include "header_2.php"; ?>
+
+<div class="row p_cont"  >
+<div class="" >
+<div class="content col-sm-9" >
+<a name=main ></a>
+<div id="main" class=main >
+
+<?php include 'services/build_your_brand/feature/video.php'; ?>
+
+<?php include 'timeline.php'; ?>
+
+</div>
+</div>
+<div class="col-sm-3" >
+<div id=right_pane class="right_pane" >
+<div id=left_pane_1 >
+<?php include "services/sell/products/feature/group_four.php"; ?>
+</div>
+<div id=right_pane_2 style="display:none;" >
+<?php include "feature/relationship.php"; ?>
+</div>
+</div>
+</div>
+</div>
+</div>
+<?php include "footer_3.php"; ?>
+</body>
+</html>
+<?php
+$conn->close();
+?>
