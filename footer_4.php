1253bf5d89f7d7bf75202601f3c3f1bace2f5902
Mngz <mngz636@gmail.com>
2020-10-04 16:25:42 +0200

    Update footer_4.php
    line 15 customer service
    
      <li><a href=https://productlists.co.za/customer_service/faq.html >FAQ</a></li>
        <li><a href=https://productlists.co.za/customer_service/privacy_policy.html >Privacy Policy</a></li>
        <li><a href=https://productlists.co.za/customer_service/terms_conditions.html >Terms & Conditions</a></li>
        <li><a href=https://productlists.co.za/customer_service/shipping_returns.html >Shipping & Returns</a></li>

--- footer_4.php
+++ footer_4.php
@@ -1,3 +1,57 @@
-<?php
-
-?>
+<div class=row style="padding-bottom:20px;padding-left:20px;rgba(0,0,0,0.2);
+box-shadow: -1px -1px 7px 0px rgba(0,0,0,0.2);" >
+<div class="col-sm-1" >
+</div>
+<div class="col-sm-2" >
+<h3>Our Company</h3>
+<ul style="list-style:none;" >
+    <li><a href=https://www.productlists.co.za/feature/support.php >Support</a></li>
+    <li><a href=https://www.productlists.co.za/about.php >About</a></li>
+    <li><a href=https://www.productlists.co.za/feature/device_usage/ >Usage</a></li>
+  </ul>
+</div>
+<div class="col-sm-2" >
+<h3>Customer Service</h3>
+  <ul style="list-style:none;" >
+    <li><a href=https://productlists.co.za/customer_service/faq.html >FAQ</a></li>
+    <li><a href=https://productlists.co.za/customer_service/privacy_policy.html >Privacy Policy</a></li>
+    <li><a href=https://productlists.co.za/customer_service/terms_conditions.html >Terms & Conditions</a></li>
+    <li><a href=https://productlists.co.za/customer_service/shipping_returns.html >Shipping & Returns</a></li>
+  </ul>
+</div>
+<div class="col-sm-2" >
+<h3>Suppliers</h3>
+  <ul style="list-style:none;" >
+    <li><a href=https://www.productlists.co.za/services/sell >Add Company</a></li>
+    <li><a href=https://www.productlists.co.za/services/sell/products/product_general.php >Sell Product</a></li>
+    <li><a href=https://www.productlists.co.za/services/sell/company_transactions.php >Transactions</a></li>
+  </ul>
+</div>
+<div class="col-sm-2" >
+<h3>Members</h3>
+<ul style="list-style:none;" >
+    <li><a href=https://www.productlists.co.za/member/signup_main.php >Account</a></li>
+    <li><a href=https://www.productlists.co.za/feature/shopping_cart/index.php >Shopping Cart</a></li>
+    <li><a href=https://www.productlists.co.za/member/customer_transaction.php >Transactions</a></li>
+    <li><a href=https://www.productlists.co.za/services/ >Services</a></li>
+  </ul>
+</div>
+<div class="col-sm-3" >
+<h3>Newsletter</h3>
+<form action="https://www.productlists.co.za/feature/feedback/index.php" method=post onsubmit=setDate('date_added_n'); id=newsletter >
+<a href=https://www.instagram.com/productlists ><img src=https://www.productlists.co.za/feature/shopping_cart/affiliate_logos/instagram.jpg width=20px /></a>
+<a href=https://www.facebook.com/productlists.co.za ><img src=https://www.productlists.co.za/feature/shopping_cart/affiliate_logos/facebook.png width=20px /></a>
+    <input name=email type=text class=form-control placeholder=email style="margin-bottom:10px;width:70%;display:inline-block;" onblur="newsletter_auto_send(this);" />
+<script>
+    function newsletter_auto_send(input){
+        if(input.value){
+            e('newsletter').submit();
+        }
+    }
+</script>
+    
+    <input name=date_added_n type=text id=date_added class=invisible />
+    <input name=text value="Newsletter" placeholder=message class=invisible />
+</form>
+</div>
+</div>
