11f4fb9b91364405f5dc30140ad43e85dc5d5527
Mngz <mngz636@gmail.com>
2020-10-14 22:42:52 +0200

    Update delete_product.php

--- services/sell/products/delete_product.php
+++ services/sell/products/delete_product.php
@@ -3,11 +3,41 @@ session_start();
 if(ISSET($_SESSION['company_id'])){
 $conn = new mysqli('localhost','produc10_mng','mngzpass636','produc10_productlists');
 
-$sql = 'DELETE FROM product WHERE id='.$_GET['product_id'];
+$sql = 'SELECT title,price FROM product WHERE id='.$_GET['product_id'].';';
 $result = $conn->query($sql);
 if($result){
-echo 'ff_ff';
-$sql = 'SELECT id FROM product WHERE id>'.$_GET['product_id'].';';
+ $row = $result->fetch_assoc();
+ 
+ $sql = 'UPDATE transaction SET status="Deleted - Product '.$row['title'].' Price'.$row['price'].'",product_id=-1 WHERE product_id='.$_GET['product_id'];
+$conn->query($sql);
+ 
+ $sql = 'DELETE FROM product WHERE id='.$_GET['product_id'];
+$result = $conn->query($sql);
+ 
+$sql = 'SELECT url FROM product_image WHERE product_id='.$_GET['product_id'].';';
+$result = $conn->query($sql);
+if($result){
+while($row = $result->fetch_assoc()){
+ if(strpos($row['url'],'https://')==-1){
+  unlink('product_images/'.$row['url']); 
+ } 
+}
+}
+
+if($result){
+$sql = 'DELETE FROM product_image WHERE product_id='.$_GET['product_id'].';';
+$result = $conn->query($sql);
+if($result){
+$sql = 'DELETE FROM review WHERE product_id='.$_GET['product_id'].';';
+$result = $conn->query($sql);
+if($result){
+$sql = 'DELETE FROM feeling WHERE product_id='.$_GET['product_id'].';';
+$result = $conn->query($sql);
+}
+}
+}
+ 
+ $sql = 'SELECT id FROM product WHERE id>'.$_GET['product_id'].';';
 $result = $conn->query($sql);
 if($result){
 while($row = $result->fetch_assoc()){
@@ -22,31 +52,7 @@ while($row = $result->fetch_assoc()){
 }
 }
 
-$sql = 'SELECT url,title,price FROM product_image WHERE product_id='.$_GET['product_id'].';';
-$result = $conn->query($sql);
-if($result){
-if($row = $result->fetch_assoc()){
-  unlink($row['url']); 
-  
-$sql = 'UPDATE transaction SET status="Deleted - Product '.$row['title'].' Price'.$row['price'].'",product_id=-1 WHERE product_id='.$_GET['product_id'];
-$conn->query($sql);
-}
-}
-
-
-
-if($result){
-$sql = 'DELETE FROM product_image WHERE product_id='.$_GET['product_id'].';';
-$result = $conn->query($sql);
-if($result){
-$sql = 'DELETE FROM review WHERE product_id='.$_GET['product_id'].';';
-$result = $conn->query($sql);
-if($result){
-$sql = 'DELETE FROM feeling WHERE product_id='.$_GET['product_id'].';';
-$result = $conn->query($sql);
-}
-}
-}
+ 
 }
 $conn->close();
 echo $result;
