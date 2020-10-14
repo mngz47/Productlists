3155117c4e4cac47866278c0d21b0068fe39d980
Mngz <mngz636@gmail.com>
2020-10-14 17:45:18 +0200

    Update face_2.php

--- services/face_2.php
+++ services/face_2.php
@@ -1,15 +1,30 @@
 <iframe id=tv src="https://productlists.co.za/services/f_snake/index.html" style="width:100%;height:400px;border:2px solid yellow;" ></iframe>
+<div class=row >
+<div style="width:80%;float:left;" >
+<a id=tv_title href=# class="block" style="background-repeat:no-repeat;background-size:50px auto;background-postion:0 0;padding-left:55px;display:block;width:100%;" >F Snake</a>
+</div>
+<div style="width:18%;float:left;" >
+<a href=# onclick="next();return false;" class="block" style="width:100%;" >Next</a>
+</div>
+</div>
 <script>
 	var apps = 
-	    [ "https://productlists.co.za/services/f_snake/index.html",
+	    [ "https://www.productlists.co.za/services/f_snake/index.html",
 	      "https://www.productlists.co.za/services/pamel_slots/index.html",
 	      "https://www.productlists.co.za/services/kasi_nametest/index.html",
 	      "https://productlists.co.za/services/cv_engine/templates/temp32.html"];
+	
+	var titles = [ "F Snake",
+		      "Pamel Slots",
+		      "Kasi Nametest",
+		      "Temp22" ];
 	
 	var index = 0;
 	function next(){
 		if(index<apps.length){
 			e('tv').src = apps[index];
+			e('tv_title').innerHTML = titles[index];
+			e('tv_title').href = apps[index];
 			index++;
 		}else{
 		index = 0;	
@@ -17,4 +32,3 @@
 	}
 	
 	</script>
-	<a href=# onclick=next(); class=block style="float:right;" >Another One</a>
