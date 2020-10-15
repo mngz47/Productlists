cc5bceeb0f71b0372d438162ae20aaa44b626b56
Mngz <mngz636@gmail.com>
2020-10-15 07:09:40 +0200

    Update open.php

--- services/sell/products/open.php
+++ services/sell/products/open.php
@@ -131,13 +131,135 @@ if(ISSET($_SESSION['response'])){
 <div class="content col-sm-7" >
    
 
+	<?php
+
+$specification;
+	
+$sql = 'SELECT p.id,p.company_id,p.title,p.quantity,p.price,p.brand,p.specification,p.parameters,p.measurement,p.discount,DATE_FORMAT(p.date_added,"%d-%m-%y  %h:%i %p") AS date_time FROM'.
+' product p  WHERE p.id='.$product_id;
+   
+
+if($result = $conn->query($sql)){
+    
+if($row = $result->fetch_assoc()){
+	
+	$more_than_one_product = false;
+	
+	
+	$sql2 = "SELECT url FROM product_image WHERE product_id=".$row["id"];
+	$result2 = $conn->query($sql2);
+	$row2 = $result2->fetch_assoc();
+	
+	if($result2){
+
+		echo '<div id=product_images >';
+	while($row22=$result2->fetch_assoc()){
+		 $more_than_one_product = true;
+	echo '<span id="'.(strpos($row22["url"],'//')!==false?$row22["url"]:'product_images/'.$row22["url"]).'" /></span>';
+	}
+	   echo '</div>';
+    }
+	
+$sql3 = 'SELECT DISTINCT COUNT(t.product_id) AS rank FROM'.
+' transaction t WHERE t.product_id='.$row['id'];
+	$result3 = $conn->query($sql3);
+	$row3 = $result3->fetch_assoc();
+	
+          echo 
+		   '</div><div class="col-sm-8" >'.
+		   '<div class="scroll_x force_right" ><div class="variance_p" >';
+		    
+    if(isset($_SESSION['company_id'])?$_SESSION['company_id']==$row['company_id']:false){
+	echo
+	'<table class=company_options >'.
+    '<td><a href="product_general.php?product_id='.$row['id'].'" >edit</a></td>'.
+	'<td><a href=# onclick="deleteProduct('.$row['id'].');return false;" >delete</a></td>'.
+	'</table>';
+	}
+		   
+		   echo
+		   '<a class=title href="products.php?product_id='.$row['id'].'" >'.$row['title'].'</a>'.
+		   '<table class=p_head >'.
+		   '<tr>'.
+		   '<td class=price title=price >'.
+		   'R'.$row['price'].
+		   '</td>'.
+		   '<td class=discount title=discount >'.($row['discount']?'-'.round($row['discount'],2).'%':'').'</td>'.
+		   '<td class=sold title=sold >'.
+		   ($row3['rank']?$row3['rank'].'<span class=caret ></span>':'').
+		   '</td>'.
+		   '</tr>'.
+		   '</table>'.
+		   '<a href="products.php?product_id='.$row['id'].'" ><img id=main_product_image class=main_product_image onmouseleave=" e(\'mother\').style.display = \'none\';" onmousemove="showMother(event);" src="'.(strpos($row2["url"],'//')!==false?$row2["url"]:'product_images/'.$row2["url"]).'" /></a>'.
+		   
+		   (ISSET($more_than_one_product)?
+		   '<div style="text-align:center;" ><a href=# onclick="nextProduct();return false;" class=next_product > > </a></div>':'');
+		   
+		$result2 = $conn->query("SELECT logo,name,website FROM company WHERE id=".$row["company_id"]);
+	       if($result2){
+           $row2 = $result2->fetch_assoc();
+		   echo 
+		  
+           '<table class=p_head >'.
+		   '<tr>'.
+		   '<td>'.
+		   '<a class=company href="products.php?brand='.$row['brand'].'" >'.$row["brand"].'</a>'.
+		   '</td>'.
+		   '<td>'.($category=='grocery'?explode(';',$row['measurement'])[0].($row['title']=='Cooking Oil' || $row['title']=='Milk' || $row['title']=='Mass'?'lt':'kg'):'').'</td>'.
+		   '<td>'.
+		   '<span class=date ><a class=fshare href="https://facebook.com/sharer.php?u=https://www.productlists.co.za/services/sell/products/open.php?product_id=
+'.$row['id'].'" onclick="javascript:window.open(this.href, \'\', \'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600,top=150, left=\'+((screen.width/2)-300));return false;" >Share</a></span>'.
+		   '</td>'.
+		   '</tr>'.
+		   '</table>';
+		   }
+		   
+		   echo 
+		   '<div class=params id=params >';
+		   
+		   $params = explode('/',$row['parameters']);
+		   for($a=0;$a<count($params)-1;$a++){
+		   $param_name = explode('>',$params[$a])[0];
+		   echo '<select><option value="'.$param_name.'" >choose '.$param_name.'</option>';
+		   
+		   
+		   $param_values = explode(';',explode('>',$params[$a])[1]);
+		   
+		   for($aa=0;$aa<count($param_values)-1;$aa++){
+		   echo '<option value="'.$param_values[$aa].'" >'.$param_values[$aa].'</option>';
+		   }
+		   echo '</select>';
+		   }
+		   
+		   echo 
+		   '</div>'.
+		   '<script>checkImageParameterAbility();</script>'.
+		   '<div class="center block" >'.($category=='grocery'?'Specialized grocery delivery.':'Expect delivery within 2 weeks.').'</div>'.
+		   
+		   
+		   ($row['quantity']>0?
+		   '<div class=add_to_cart ><input id=cart_qty type=number value=1 placeholder="Qty" /><a class="btn btn-primary" id=add_to_cart href=# onclick="addToCart(setParameters('.$row['id'].'),this);return false;" >add to cart</a></div>':'<div class="block center" >Out of Stock</div>');
+		  
+	         $specification = $row['specification'];
+			 
+           echo 
+		   '</div>';
+		   
+		   echo
+		   '</div></div></div>'; 
+		   
+    }
+}
+	
+?>
+	
 	<div id=tabs >
 		<a href=# onclick="e('checkout').style.display='none';e('reviews').style.display='none';e('spec').style.display='';" class="space" >Description</a>
 		<a href=# onclick="e('checkout').style.display='';e('reviews').style.display='none';e('spec').style.display='none';" class="space" >Checkout</a>
 		<a href=# onclick="e('checkout').style.display='none';e('reviews').style.display='';e('spec').style.display='none';" class="space" >Reviews</a>
 	</div>
-	
-	<div id=spec >
+	<div style="padding:10px;" >
+		<div id=spec >
 	<?php echo '<p class=spec >'.$specification.'</p>';  ?>
 	</div>
 <div id=checkout style="display:none;" >
@@ -165,13 +287,15 @@ include str_replace('\\','/',$_SERVER['DOCUMENT_ROOT'])."/services/sell/products
 include str_replace('\\','/',$_SERVER['DOCUMENT_ROOT'])."/services/sell/products/reply/list.php";
 ?>
 </div>
+	</div>
+	
 	
 </div>
 <div class="col-sm-3" >
 
 </div>
 </div>
-	<div class=row >
+	<div class=row style="padding:20px;" >
 		<div class=col-sm-4 ><?php include (str_replace('\\','/',$_SERVER['DOCUMENT_ROOT']).'/services/sell/products/feature/group_four_affiliate.php'); ?>
 </div>
 		<div class=col-sm-4 >
