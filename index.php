eb156ac46bd7eda97a7e8a6f2e909d64bdee605a
Mngz <mngz636@gmail.com>
2020-10-26 01:21:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -67,7 +67,10 @@ if(ISSET($_SESSION['response'])){
 		     "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=26&l=ur1&category=amazonhomepage&f=ifr&linkID=395a3797c0390ce90b0909dac625fbf4&t=productlists4-20&tracking_id=productlists4-20",
 		     "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=26&l=ur1&category=bestsellingproducts&banner=1F8VTV49P7N5K5XY5N82&f=ifr&linkID=62fbaa2c4f1eadfd296f4e00a685b4c2&t=productlists4-20&tracking_id=productlists4-20",
 		     "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=48&l=ur1&category=instruments&banner=1F5XAJEA6KXT7JPXEC02&f=ifr&linkID=e4ae1c402faaed3fc1483d4072b07ade&t=productlists4-20&tracking_id=productlists4-20",
-		     "https://www.productlists.co.za/services/air_bnb/index.html"];
+		         "https://www.productlists.co.za/services/banners/air_bnb.html",
+			 "https://www.productlists.co.za/services/banners/temp222.html",
+			 "https://www.productlists.co.za/services/banners/ezio.html"];
+	    
     var index_home = 0;
     
     function four_next_home(){
