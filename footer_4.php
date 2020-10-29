800fdfb9a7577c33b30e7f126a64607d9541e50b
Mngz <mngz636@gmail.com>
2020-10-29 14:21:10 +0200

    Update footer_4.php

--- footer_4.php
+++ footer_4.php
@@ -5,7 +5,7 @@ box-shadow: -1px -1px 7px 0px rgba(0,0,0,0.2);" >
 <ul style="list-style:none;" >
     <li><a href=https://www.productlists.co.za/feature/support.php >Support</a></li>
     <li><a href=https://www.productlists.co.za/about.php >About</a></li>
-    <li><a href=https://www.productlists.co.za/feature/aggregation/lite >Automatic</a></li>
+    <li><a href=https://www.productlists.co.za/feature/aggregation/lite/index.html >Automatic</a></li>
     <li><a href=https://www.productlists.co.za/invest.php >Invest</a></li>
   </ul>
 </div>
