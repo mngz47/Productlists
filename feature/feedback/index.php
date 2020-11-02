120e11b382409dff2eb1636966de0cff96b4e505
Mngz <mngz636@gmail.com>
2020-11-02 15:41:00 +0200

    Update index.php

--- feature/feedback/index.php
+++ feature/feedback/index.php
@@ -1,26 +1,33 @@
 <?php
 
-include str_replace('\\','/',$_SERVER['DOCUMENT_ROOT']).'/api.php';
+// include str_replace('\\','/',$_SERVER['DOCUMENT_ROOT']).'/api.php';
 
-session_start();
+// session_start();
 
 // p_mail('mngz636@gmail.com','Productlists Feedback',$_POST['email'].'>>>'.$_POST['text']);
 
 //-----------------------------------
 
-require 'vendor/autoload.php';
-use Mailgun\Mailgun;
+require 'vendor/autoload.php'; // If you're using Composer (recommended)
 
-# Instantiate the client.
-$mgClient = new Mailgun('f6f26b4383b27ab64c8c7921a387746b-ea44b6dc-07d85e16');
-$domain = "sandboxc720bc57eb9e4e1db3d3473679e23e14.mailgun.org";
+$email = new \SendGrid\Mail\Mail(); 
+$email->setFrom("test@example.com", "Example User");
+$email->setSubject("Sending with SendGrid is Fun");
+$email->addTo("mngz636@gmail.com", "Example User");
+$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid(getenv('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'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}
 
-# Make the call to the client.
-$result = $mgClient->sendMessage("$domain",
-	array('from'    => 'Mailgun Sandbox <postmaster@sandboxc720bc57eb9e4e1db3d3473679e23e14.mailgun.org>',
-		  'to'      => 'Mongezi <mngz636@gmail.com>',
-		  'subject' => 'Hello Mongezi',
-		  'text'    => 'Congratulations Mongezi, you just sent an email with Mailgun!  You are truly awesome! '));
 
 
 //-------------------------------
