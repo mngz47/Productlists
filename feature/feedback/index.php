0e364e8c25a75d697acf0652582a13b6c885884b
Mngz <mngz636@gmail.com>
2020-11-02 16:09:12 +0200

    Update index.php

--- feature/feedback/index.php
+++ feature/feedback/index.php
@@ -8,28 +8,36 @@
 
 //-----------------------------------
 
+$curl = curl_init();
 
-$client = new httpClient;
-$request = new httpClientRequest;
-
-$body = new httpMessageBody;
-$body->append('{  "personalizations": [    {      "to": [        {          "email": "mngz636@gmail.com"        }      ],      "subject": "Hello, World!"    }  ],  "from": {    "email": "from_address@example.com"  },  "content": [    {      "type": "text/plain",      "value": "Hello, World!"    }  ]}');
-
-$request->setRequestUrl('https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send');
-$request->setRequestMethod('POST');
-$request->setBody($body);
-
-$request->setHeaders(array(
-	'x-rapidapi-host' => 'rapidprod-sendgrid-v1.p.rapidapi.com',
-	'x-rapidapi-key' => 'f4d041c051msh25be51a74caa34bp14fd74jsn556f558ca5da',
-	'content-type' => 'application/json',
-	'accept' => 'application/json'
+curl_setopt_array($curl, array(
+	CURLOPT_URL => "https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "POST",
+	CURLOPT_POSTFIELDS => "{  \"personalizations\": [    {      \"to\": [        {          \"email\": \"mngz636@gmail.com\"        }      ],      \"subject\": \"Hello, World!\"    }  ],  \"from\": {    \"email\": \"from_address@example.com\"  },  \"content\": [    {      \"type\": \"text/plain\",      \"value\": \"Hello, World!\"    }  ]}",
+	CURLOPT_HTTPHEADER => array(
+		"accept: application/json",
+		"content-type: application/json",
+		"x-rapidapi-host: rapidprod-sendgrid-v1.p.rapidapi.com",
+		"x-rapidapi-key: f4d041c051msh25be51a74caa34bp14fd74jsn556f558ca5da"
+	),
 ));
 
-$client->enqueue($request)->send();
-$response = $client->getResponse();
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-echo $response->getBody();
+curl_close($curl);
+
+if ($err) {
+	echo "cURL Error #:" . $err;
+} else {
+	echo $response;
+}
 
 //-------------------------------
 
