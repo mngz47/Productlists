0d84e571ea02f9d87e87a5996f7d648563772ebc
Mngz <mngz636@gmail.com>
2020-11-02 15:03:03 +0200

    Update index.php

--- feature/feedback/index.php
+++ feature/feedback/index.php
@@ -6,20 +6,24 @@ session_start();
 
 // p_mail('mngz636@gmail.com','Productlists Feedback',$_POST['email'].'>>>'.$_POST['text']);
 
+//-----------------------------------
 
-$this->load->library('google');
+require 'vendor/autoload.php';
+use Mailgun\Mailgun;
 
-try {
-    $message = new Message();
-    $message->setSender('admin@productlists.co.za');
-    $message->addTo('mngz636@gmail.com');
-    $message->setSubject('Productlists Feedback');
-    $message->setTextBody(,$_POST['email'].'>>>'.$_POST['text']);
-    $message->send();
-    echo 'Mail Sent';
-} catch (InvalidArgumentException $e) {
-    echo 'There was an error';
-}
+# Instantiate the client.
+$mgClient = new Mailgun('f6f26b4383b27ab64c8c7921a387746b-ea44b6dc-07d85e16');
+$domain = "sandboxc720bc57eb9e4e1db3d3473679e23e14.mailgun.org";
+
+# Make the call to the client.
+$result = $mgClient->sendMessage("$domain",
+	array('from'    => 'Mailgun Sandbox <postmaster@sandboxc720bc57eb9e4e1db3d3473679e23e14.mailgun.org>',
+		  'to'      => 'Mongezi <mngz636@gmail.com>',
+		  'subject' => 'Hello Mongezi',
+		  'text'    => 'Congratulations Mongezi, you just sent an email with Mailgun!  You are truly awesome! '));
+
+
+//-------------------------------
 
 if(ISSET($_POST['email']) && ISSET($_POST['text']) && ISSET($_POST['date_added'])   && !ISSET($_SESSION['feedback_lock'])){
 
