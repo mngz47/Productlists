34017dd76ad51d4a1bb37f46f939e290845ee723
Mngz <mngz636@gmail.com>
2020-11-03 09:20:07 +0200

    Update index.php

--- feature/feedback/index.php
+++ feature/feedback/index.php
@@ -8,19 +8,6 @@
 
 //-----------------------------------
 
-require 'vendor/autoload.php';
-use Mailgun\Mailgun;
-
-# Instantiate the client.
-$mgClient = new Mailgun('f6f26b4383b27ab64c8c7921a387746b-ea44b6dc-07d85e16');
-$domain = "sandboxc720bc57eb9e4e1db3d3473679e23e14.mailgun.org";
-
-# Make the call to the client.
-$result = $mgClient->sendMessage("$domain",
-	array('from'    => 'Mailgun Sandbox <postmaster@sandboxc720bc57eb9e4e1db3d3473679e23e14.mailgun.org>',
-		  'to'      => 'Mongezi <mngz636@gmail.com>',
-		  'subject' => 'Hello Mongezi',
-		  'text'    => 'Congratulations Mongezi, you just sent an email with Mailgun!  You are truly awesome! '));
 
 
 //-------------------------------
