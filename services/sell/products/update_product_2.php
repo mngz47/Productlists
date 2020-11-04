f59e73567ff00d4ff7e8a5d71fd87ad2554060c0
Mngz <mngz636@gmail.com>
2020-11-04 13:43:32 +0200

    Create update_product_2.php

---
+++ services/sell/products/update_product_2.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+if(ISSET($_SESSION['company_id'])){
+$conn = new mysqli('localhost','produc10_mng','mngzpass636','produc10_productlists');
+
+if($_GET['fieldname']==='price'){
+$sql = 'UPDATE product SET discount=((price-'.$_POST[$_GET['fieldname']]).')/price*100) WHERE id='.$_GET['id'].';';
+$result = $conn->query($sql);
+}
+
+$sql = 'UPDATE product SET '.$_GET['fieldname'].'='.str_replace('\"','"',$_POST[$_GET['fieldname']]).' WHERE id='.$_GET['id'].';';
+$result = $conn->query($sql);
+
+$conn->close();
+echo ($result?'success':'error with query : '.$sql);
+}
+?>
