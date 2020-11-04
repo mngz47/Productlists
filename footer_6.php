b7377052fbb308e1ff652fcfd5ff9b1f57ffa68d
Mngz <mngz636@gmail.com>
2020-11-04 11:37:08 +0200

    Create footer_6.php

---
+++ footer_6.php
@@ -0,0 +1,45 @@
+<div style="border-top:2px red solid;" >
+<div class="col-sm-3" >
+
+    <table class=affiliates >
+        <tr>
+            <td><img src=https://www.productlists.co.za/feature/shopping_cart/affiliate_logos/pay_fast.png /></td>
+            <td><img src=https://www.productlists.co.za/feature/shopping_cart/affiliate_logos/payment/fnb.png /></td>
+            <td><img src=https://www.productlists.co.za/feature/shopping_cart/affiliate_logos/payment/master_card.png /></td>
+            <td><img src=https://www.productlists.co.za/feature/shopping_cart/affiliate_logos/payment/paypal.png /></td>
+            <td><img src=https://www.productlists.co.za/feature/shopping_cart/affiliate_logos/payment/visa.png /></td>
+        </tr>
+        <tr style="border-top:2px black solid;" ><td></td><td></td><td>Affiliates</td><td></td><td></td></tr>
+    </table>
+<style>
+    .affiliates img {
+        width:40px;
+    }
+    .affiliates{
+        margin-top:10px;
+        margin-left:auto;
+        margin-right:auto;
+    }
+</style>
+</div>
+<div class="col-sm-6 center" >
+<h3>Newsletter</h3>
+<form action="https://www.productlists.co.za/feature/feedback/index.php" method=post onsubmit=setDate('date_added'); >
+<a href=https://www.instagram.com/productlists ><img src=https://www.productlists.co.za/feature/shopping_cart/affiliate_logos/instagram.jpg width=20px /></a>
+<a href=https://www.facebook.com/productlists.co.za ><img src=https://www.productlists.co.za/feature/shopping_cart/affiliate_logos/facebook.png width=20px /></a>
+    <input name=email type=text class=form-control placeholder=email style="margin-bottom:10px;width:70%;display:inline-block;" />
+    <button class="btn btn-primary" >Subscribe</button>
+
+    <input name=date_added type=text id=date_added class=invisible />
+    <input name=text value="Newsletter" placeholder=message class=invisible />
+</form>
+</div>
+<div class="col-sm-3" >
+<div class="scroll_text_cont" >
+<a class="scroll_text" href=https://www.productlists.co.za/services/sell/products/products.php >Products</a>
+<a class="scroll_text" href=https://www.productlists.co.za/services/build_your_brand/columns.php >Columns</a>
+<a class="scroll_text" href=https://www.productlists.co.za/services/directory_listings/company_directory.php >Directory</a>
+<a class="scroll_text" href=https://www.productlists.co.za/feature/support.php >Support</a>
+</div>
+</div>
+</div>
