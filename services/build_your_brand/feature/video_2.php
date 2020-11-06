cf2478ff547ce12271b165e2498b15d4e829d447
Mngz <mngz636@gmail.com>
2020-11-06 03:39:05 +0200

    Update video_2.php

--- services/build_your_brand/feature/video_2.php
+++ services/build_your_brand/feature/video_2.php
@@ -1,8 +1,8 @@
 <div class="video_plugin w3-animate-left" >
-<iframe id=tv width="100%" ></iframe>
+<iframe id=yt_tv width="100%" ></iframe>
 <div class=row >
 <div style="width:80%;float:left;" >
-<a id=tv_title href=# class="block" style="background-repeat:no-repeat;background-size:50px auto;background-postion:0 0;padding-left:55px;display:block;width:100%;" ></a>
+<a href=# onclick="nextVideo();return false;" class="block" style="background-repeat:no-repeat;background-size:50px auto;background-postion:0 0;padding-left:55px;display:block;width:100%;" ></a>
 </div>
 <div style="width:18%;float:left;" >
 <a href=# onclick="nextVideo();return false;" class="block" style="width:100%;" >next video</a>
@@ -10,10 +10,10 @@
 </div>
 <script>
      
-e('tv').style.height = (e('tv').offsetWidth*220/400)+'px';
+e('yt_tv').style.height = (e('yt_tv').offsetWidth*220/400)+'px';
     
-e('tv').onresize = function(){
-e('tv').style.height = (e('tv').offsetWidth*220/400)+'px';
+e('yt_tv').onresize = function(){
+e('yt_tv').style.height = (e('yt_tv').offsetWidth*220/400)+'px';
     };
     
 var video_index = 0;
@@ -23,15 +23,15 @@ var videos =
 'https://www.youtube.com/embed/4uU7UIrSFlg',
 'https://www.youtube.com/embed/T4vuh_kQvcA'];
 
-e('tv').src = videos[video_index];
+e('yt_tv').src = videos[video_index];
 
 function nextVideo(){
 	if(video_index<(videos.length-1)){
 		video_index+=1;
-		 e('tv').src = videos[video_index];
+		 e('yt_tv').src = videos[video_index];
 	}else{
 		video_index=0;
-		e('tv').src = videos[video_index];
+		e('yt_tv').src = videos[video_index];
 	}
 }
 </script>
