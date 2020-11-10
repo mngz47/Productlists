41e54000dca3b75ce41ccd497e0ffb24dccaf8f2
Mngz <mngz636@gmail.com>
2020-11-10 11:10:14 +0200

    Update open.php

--- services/sell/products/open.php
+++ services/sell/products/open.php
@@ -7,14 +7,14 @@ function getQueueDiscount($product_id,$q_price){
 	$q_discount=0;
 	$sql = 'SELECT COUNT(id) AS ii FROM queue WHERE product_id='.$product_id;
 $result = $conn->query($sql);
-	      if($q_row = $result->fetch_assoc()){
-		   if($q_row['ii']){
+	    //  if($q_row = $result->fetch_assoc()){
+		//   if($q_row['ii']){
 			   
             //  $q_discount = ($price/3)($q_row['ii']/1000);
 		//   $q_discount = $q_price - $q_discount;
 			   
-		   }
-		   }
+		 //  }
+		//   }
 		   return ($q_discount?$q_discount:$q_price);
 }
 
