bbb2a3361cbdfc4718e453ae57b0cbe9623b8980
Mngz <mngz636@gmail.com>
2020-11-10 10:45:55 +0200

    Update open.php

--- services/sell/products/open.php
+++ services/sell/products/open.php
@@ -4,8 +4,8 @@ session_start();
 $conn = new mysqli('localhost','produc10_mng','mngzpass636','produc10_productlists');
 
 function getQueueDiscount($product_id,$price){
-	/*$q_discount;
-	$sql = 'SELECT COUNT(id) AS ii FROM queue WHERE product_id='.$product_id;
+	$q_discount;
+	/*$sql = 'SELECT COUNT(id) AS ii FROM queue WHERE product_id='.$product_id;
 $result = $conn->query($sql);
 	       if($result){
 			   
