f163922d181e23805a8654c6d28fd01dfbd68680
Mngz <mngz636@gmail.com>
2020-12-01 10:09:41 +0200

    Add files via upload

---
+++ auto.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Auto</title></head>
+<link rel=stylesheet href=https://productlists.co.za/bootstrap.min.css />
+<body>
+<div style="text-align:center;" >
+<h3>Sponsored Video</h3>
+<iframe width="640" height="360" src="https://www.youtube.com/embed/w4LMvqk-MVE?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<br>
+<br>
+<a class="btn btn-primary" href="<?php echo $_GET['skip']; ?>" >Skip</a>
+</div>
+</body>
+</html>
