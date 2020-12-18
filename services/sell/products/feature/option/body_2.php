acab82b0b8bed60d0b80aff9e2b5a6a1bf9280b9
Mngz <mngz636@gmail.com>
2020-12-18 18:53:40 +0200

    Update body_2.php

--- services/sell/products/feature/option/body_2.php
+++ services/sell/products/feature/option/body_2.php
@@ -14,19 +14,17 @@
 	    
 	if($result3=($conn->query("SELECT id,title FROM product WHERE category='".$row2['category']."' ORDER BY date_added DESC"))){
 		while($row3 = $result3->fetch_assoc()){
-			if($five_brands<10){
+		if($five_brands<10){
 				
 			if($result4=($conn->query('SELECT url FROM product_image WHERE product_id='.$row3['id']))){
-		    if($row4 = $result4->fetch_assoc()){
+		    	if($row4 = $result4->fetch_assoc()){
 				 
 			echo 
 			 '<a class="row w3-container w3-center w3-animate-zoom" style="background-position:0 20;background-repeat:no-repeat;background-size:100%;background-image:url('.(strpos($row4['url'],'http')!==false?$row4['url']:'https://www.productlists.co.za/services/sell/products/product_images/'.str_replace (' ','%20',$row4['url'])).');display:inline-block;width:110px;height:150px;margin:3px;" '.
 			 ' href="services/sell/products/open.php?product_id='.$row3['id'].'" ><span class=tint >'.$row3['title'].
 			 '</span></a>';
 			}
-	        }	
-			}else{
-			break;
+	        	}	
 		}
 			$five_brands+=1;
 		}
