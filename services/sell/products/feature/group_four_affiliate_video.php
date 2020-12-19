f54d3a865f85b6185c1aecedafc65762aeaeeb99
Mngz <mngz636@gmail.com>
2020-12-19 04:56:59 +0200

    Update group_four_affiliate_video.php

--- services/sell/products/feature/group_four_affiliate_video.php
+++ services/sell/products/feature/group_four_affiliate_video.php
@@ -1,7 +1,6 @@
 <iframe id=four_target style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B07K39FRSL&asins=B07K39FRSL&linkId=065aee4d4dac6dbbe3ad4fe7adddc370&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff">
     </iframe>
 <div class=forward >
-    <a href=# id=four_forward onclick="four_next();return false;" class=block >  </a>
     <script>
    
     var affiliates = [
