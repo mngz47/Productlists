08677cb46ab29e18906bed4cd177580868c7b20d
Mngz <mngz636@gmail.com>
2020-12-19 14:40:04 +0200

    Update video_2.php

--- services/build_your_brand/feature/video_2.php
+++ services/build_your_brand/feature/video_2.php
@@ -20,10 +20,19 @@ var video_index = 0;
 
 var videos = 
 [
+'https://www.youtube.com/embed/HjDiASNwExU'
 'https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fweb.facebook.com%2Fproductlists.co.za%2Fvideos%2F378822863181119%2F&show_text=false&width=560',
 'https://www.youtube.com/embed/B4RgQEuzcLY',
 'https://www.youtube.com/embed/4uU7UIrSFlg',
-'https://www.youtube.com/embed/T4vuh_kQvcA'];
+'https://www.youtube.com/embed/T4vuh_kQvcA',
+'https://www.youtube.com/embed/bDqhFfTObsw',
+'https://www.youtube.com/embed/2HSbisFCLrk',
+'https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fweb.facebook.com%2Fproductlists.co.za%2Fvideos%2F830921624389241%2F&show_text=false&width=560',
+'https://www.youtube.com/embed/57kS-89h9-E',
+'https://www.youtube.com/embed/Joi5lIH3E4g',
+'https://www.youtube.com/embed/XDv6u7Pw3m4',
+'https://www.youtube.com/embed/lmrqxiq83pM'
+];
 
 e('yt_tv').src = videos[video_index];
 	
