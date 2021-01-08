4b1aa4596d2a65e3b8abeab119df38527251b76d
Mngz <mngz636@gmail.com>
2021-01-08 20:59:25 +0200

    Update open.php

--- services/sell/products/open.php
+++ services/sell/products/open.php
@@ -355,7 +355,14 @@ include str_replace('\\','/',$_SERVER['DOCUMENT_ROOT'])."/services/sell/products
 </div>
 	</div>
 	<div class=row style="padding:20px;" >
-		<div class=col-sm-4 ><?php include (str_replace('\\','/',$_SERVER['DOCUMENT_ROOT']).'/services/sell/products/feature/group_four_affiliate.php'); ?>
+		<div class=col-sm-4 >
+<div id=left_pane_1 >
+<?php include (str_replace('\\','/',$_SERVER['DOCUMENT_ROOT']).'/services/sell/products/feature/group_four.php'); ?>
+</div>
+<div id=left_pane_3 >
+<?php include (str_replace('\\','/',$_SERVER['DOCUMENT_ROOT']).'/services/sell/products/feature/group_four_control.php'); ?>
+</div>	
+			
 </div>
 		<div class=col-sm-4 >
 
@@ -378,6 +385,73 @@ include str_replace('\\','/',$_SERVER['DOCUMENT_ROOT']).'/services/cover_face.ph
 ?>
 	</div>
 		</div>
+	
+	<div class="row" >
+<div class="content col-sm-9" >
+	<div style="padding:20px;" >
+		<div id='amazon_products' style='display:none' >
+		<span class=title >Introduction To Productlists</span>
+		<p>Productlists is a tech firm working towards solutions to free the youth. Enjoy our instant game service, product listings and reviews.</p>	
+		<span class=title >Workx Vacuum Cleaner</span>
+		<p>Portable with strong suction good for removig dirt in narrow spaces. Good for cleaning carpets and car interior.</p>
+		<span class=title >Kasi Nametest</span>
+		<p>South African local instant game - enter your name and find out with Mzansi kasi you are from.</p>
+		<span class=title >Recreational Activies In Newcastle South Africa</span>
+		<p>Get the latest business and tourist information about Newcastle. Learn about Accommodation, Job Recruitment and more.</p>
+		<span class=title >F-Snake Classic Productlists Game</span>
+		<p>Play our classic snake game with seven stages. Overcome the obstacle and score as much points as you can.</p>
+		<span class=title >F-Snake Classic Productlists Game</span>
+		<p>Play our classic snake game. As you progress in the stages the obstacle moves faster and faster.</p>
+		<span class=title >Touch Bistro Restaurant POS</span>
+		<p>Replace your printed out food menu with a digital tablet menu able to send orders directly to kitchen without calling for waiter.</p>
+		<span class=title >Touch Bistro Digital Menu and Payment</span>
+		<p>Collect more information during payment so you can resell your specials to frequent customers.</p>
+		<span class=title >CV Engine</span>
+		<p>Generate Your CV For FREE in 3 minutes and send it to your email.</p>
+		<span class=title >Stop Paying For Traffic.</span>
+		<p>Saas and other large companies that need to pay for advertising on a monthly basis can create a content division in their businesses.</p>	
+		<span class=title >Jungle Scout</span>
+		<p>Use the best tools to sell on Amazon, optimize your listing - access best selling keywords and automatic repricing.</p>
+		<span class=title >Adzooma</span>
+		<p>Automate and manage all your Google, Facebook and Microsoft Ads in one place. Award winning App.</p>
+			<span>Apple Watch Series 3</span>
+			<p>GPS, Retina display, Swimproof, Optical heart sensor, Stores music, podcasts, and audiobooks
+Elevation, Emergency SOS, S3 chip with dual-core processor,watchOS with Activity trends, cycle tracking, hearing health innovations, and the App Store on your wrist
+Aluminum case</p>
+			<span>10'' Ring Light with 50'' Extendable Tripod Stand</span>
+			<p>
+			Sensyne selfie ring light features a high sensitivity touch panel design that has a longer life than traditional mechanical buttons.	
+			</p>
+			<span>Casio Men's W800H-1AV Classic Sport Watch</span>
+			<p>
+			Imported, 10-year battery life, 100-meter water resistance
+LED backlight, Dual time, Approx. battery life: 10 years on CR2025	
+			</p>
+			<span>All-new Echo Dot (4th Gen)</span>
+			<p>
+Ready to help - Ask Alexa to tell a joke, play music, answer questions, play the news, check the weather, set alarms, and more.	
+			</p>
+			<span>DJI Mavic Mini Combo - Drone FlyCam Quadcopter UAV</span>
+			<p>
+			Mavic Mini’s weight allows it to stay in the air longer than similar consumer FlyCams on the market Enjoy up to 30 minutes of flight time with a fully-charged battery.	
+			</p>
+			<span>Pixel 4 - Clearly White</span>
+			<p>
+			Get more done with your voice. The new Google Assistant is the easiest way to send texts, share photos, and more.	
+			</p>
+			
+		</div>
+		
+		<?php include "services/build_your_brand/feature/video_2.php"; ?>
+		
+	</div>
+		</div>
+<div class="col-sm-3" >
+<?php include "services/sell/products/feature/group_four_affiliate_video.php"; ?>
+	<div id=group_four_affiliate_video_dd style="padding:4px;font-size:1.3em;" >Productlists is a tech firm working towards solutions to free the youth. Enjoy our instant game service, product listings and reviews.</div>
+		</div>
+	</div>
+	
 <?php 	
 include str_replace('\\','/',$_SERVER['DOCUMENT_ROOT'])."/footer_4.php"; 
 ?>
