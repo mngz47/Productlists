8a5498445df5fee2b27c85838f80c4465f5f7153
Mngz <mngz636@gmail.com>
2021-02-02 13:19:15 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,7 @@ session_start();
 
 $conn = new mysqli('localhost','produc10_mng','mngzpass636','produc10_productlists');
 
-$HOME_ = 'productlists.heroku.com';
+//$HOME_ = 'productlists.heroku.com';
+$HOME_ = 'www.productlists.co.za';
 
 ?>
