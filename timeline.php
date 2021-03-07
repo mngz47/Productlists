2aa9f7f3a5ed10825e35ed2733aeba5b96c2bf8f
Mngz <mngz636@gmail.com>
2021-03-07 21:21:12 +0200

    Update timeline.php

--- timeline.php
+++ timeline.php
@@ -20,34 +20,7 @@ $conn = new mysqli($HOST_,$USER_,$PASS_,$DATABASE_);
     if($result2){
             if($row2 = $result2->fetch_assoc()){
             
-            if($c%3==0){
-    $conn = new mysqli('localhost','produc10_mng','mngzpass636','produc10_productlists_columns');
-    $sql = 'SELECT id,title,cover_image FROM col ORDER BY date_added DESC';
-    $result3 = $conn->query($sql);
-    if($result3){
-        
-        $cc = 1;
-        
-    while($row3 = $result3->fetch_assoc()){
-           if($cc==$c/3){
-              
-                
-               $inject.=
-              '<a href="https://'.$HOME_.'/services/build_your_brand/open.php?member_id='.$row3['id'].'" >'.
-             '<div class="block block_purple" style="min-height:200px;" >'.
-             '<img src="https://'.$HOME_.'/services/build_your_brand/images/'.$row3['cover_image'].'" width=100% />'.
-             '<span style="font-size:1em;" >'.$row3['title'].'</span>'.
-             '</div>'.
-             '</a>'.($c%2==0 ?'</div>'.($c%12==0?'</div><div class="row tml_row" >':'').'<div class="col-sm-6 row" >':'');
-                
-                $c+=1;
-               break;
-           }
-           $cc+=1;
-    }
-    } 
-            }
-                
+       
              $inject.=
              '<a href="https://'.$HOME_.'/services/sell/products/open.php?product_id='.$row['id'].'" >'.
              '<div class="block '.($c%4==0?'block_purple':'').' tml_p" >'.
