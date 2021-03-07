f7cbd570e88963c995a269734a15ba478a5e501c
Mngz <mngz636@gmail.com>
2021-03-08 00:18:51 +0200

    Update timeline.php

--- timeline.php
+++ timeline.php
@@ -14,18 +14,56 @@ $conn = new mysqli($HOST_,$USER_,$PASS_,$DATABASE_);
     if($result){
     while($row = $result->fetch_assoc()){	
         if($c<40){
-        $conn = new mysqli($HOST_,$USER_,$PASS_,$DATABASE_);
               $sql = 'SELECT url FROM product_image WHERE product_id='.$row['id'];
     $result2 = $conn->query($sql);
     if($result2){
             if($row2 = $result2->fetch_assoc()){
             
-       
+            if($c%3==0){
+   
+    $sql = 'SELECT id,title,price FROM product WHERE draft=0 ORDER BY date_added DESC';
+    $result3 = $conn->query($sql);
+    
+	if($result3){
+        
+        $cc = 1;
+        
+    while($row3 = $result3->fetch_assoc()){
+		
+           if($cc==$c/3){
+              
+				$sql = 'SELECT url FROM product_image WHERE product_id='.$row3['id'];
+					$result4 = $conn->query($sql);
+					if($result4){
+					if($row4 = $result4->fetch_assoc()){
+						
+						 $inject.=
+             '<a href="https://www.productlists.co.za/services/sell/products/open.php?product_id='.$row3['id'].'" >'.
+             '<div class="block '.($c%4==0?'block_purple':'').' tml_p" >'.
+             '<span class=highlight_green >R'.$row3['price'].'</span>'.
+             '<img src="'.(strpos($row4['url'],'//')!==false?$row4['url']:'https://www.productlists.co.za/services/sell/products/product_images/'.$row4['url']).'" width=100% />'.
+             '<span style="font-size:1em;" >'.$row3['title'].'</span>'.
+             '</div>'.
+             '</a>'.($c%2==0 ?'</div>'.($c%12==0?'</div><div class="row tml_row" >':'').'<div class="col-sm-6 row" >':'');
+              
+						
+					}}
+				
+                $c+=1;
+               break;
+           }
+           $cc+=1;
+    }
+    } 
+            }
+			
+      //---------------------------------------------
+	  
              $inject.=
-             '<a href="https://'.$HOME_.'/services/sell/products/open.php?product_id='.$row['id'].'" >'.
+             '<a href="https://www.productlists.co.za/services/sell/products/open.php?product_id='.$row['id'].'" >'.
              '<div class="block '.($c%4==0?'block_purple':'').' tml_p" >'.
              '<span class=highlight_green >R'.$row['price'].'</span>'.
-             '<img src="'.(strpos($row2['url'],'//')!==false?$row2['url']:'https://'.$HOME_.'/services/sell/products/product_images/'.$row2['url']).'" width=100% />'.
+             '<img src="'.(strpos($row2['url'],'//')!==false?$row2['url']:'https://www.productlists.co.za/services/sell/products/product_images/'.$row2['url']).'" width=100% />'.
              '<span style="font-size:1em;" >'.$row['title'].'</span>'.
              '</div>'.
              '</a>';
