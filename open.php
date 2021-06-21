a7d37ed24a75530c8a3cf12ca9cb59838409ba81
Mngz <mngz636@gmail.com>
2021-06-21 19:57:07 +0200

    Update open.php

--- open.php
+++ open.php
@@ -118,11 +118,12 @@ $page_meta_content = ($in_product?'|':($brand?'| You can now experience all prod
 </head>
 <body>
 
-<script src=api.js ></script>
-<script src=verification.js ></script>
+<script src=https://<?php echo $HOME_; ?>/api.js ></script>
+<script src=https://<?php echo $HOME_; ?>/verification.js ></script>
 
-<script src=feature/search/api.js ></script>
-<script src=feature/sign/api.js ></script>
+<script src=https://<?php echo $HOME_; ?>/feature/search/api.js ></script>
+<script src=https://<?php echo $HOME_; ?>/feature/sign/api.js ></script>
+<script src=https://<?php echo $HOME_; ?>/feature/sign/api.js ></script>
 <script src=https://<?php echo $HOME_; ?>/services/sell/products/api.js ></script>
 
 <script src=https://<?php echo $HOME_; ?>/feature/shopping_cart/api.js ></script>
