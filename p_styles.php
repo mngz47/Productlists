8eca630485a83bbf254b66514324e8e13dc439aa
Mngz <mngz636@gmail.com>
2021-06-22 12:47:13 +0200

    Update p_styles.php

--- p_styles.php
+++ p_styles.php
@@ -2,12 +2,14 @@
 <link rel=stylesheet href=https://<?php echo $HOME_; ?>/style.css />
 <link rel=stylesheet href=https://<?php echo $HOME_; ?>/mobstyle.css />
 <link rel=stylesheet href=https://<?php echo $HOME_; ?>/input_style.css />
+<link rel=stylesheet href=https://<?php echo $HOME_; ?>/services/sell/products/input_style.css />
 <link rel=stylesheet href=https://<?php echo $HOME_; ?>/scroll_style.css />
 <link rel=stylesheet href=https://<?php echo $HOME_; ?>/feature/sign/style.css />
 <link rel=stylesheet href=https://<?php echo $HOME_; ?>/feature/search/style.css />
 <link rel=stylesheet href=https://<?php echo $HOME_; ?>/feature/feedback/style.css />
 <link rel=stylesheet href=https://<?php echo $HOME_; ?>/verification_style.css />
 <link rel=stylesheet href=https://<?php echo $HOME_; ?>/services/grocery/grocery_style.css />
+<link rel=stylesheet href=https://<?php echo $HOME_; ?>/services/sell/style.css />
 <link rel=stylesheet href=https://<?php echo $HOME_; ?>/services/sell/products/style.css />
 <link rel=stylesheet href=https://<?php echo $HOME_; ?>/services/sell/products/feature/feeling/style.css />
 <link rel=stylesheet href=https://<?php echo $HOME_; ?>/feature/shopping_cart/style.css />
