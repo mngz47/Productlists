846dbbd631b98352859d1d5f5b495633ae9a0f3c
Mngz <mngz636@gmail.com>
2021-06-22 03:38:32 +0200

    Create p_styles.php

---
+++ p_styles.php
@@ -0,0 +1,14 @@
+<link rel=stylesheet href=https://<?php echo $HOME_; ?>/bootstrap.min.css />
+<link rel=stylesheet href=https://<?php echo $HOME_; ?>/style.css />
+<link rel=stylesheet href=https://<?php echo $HOME_; ?>/mobstyle.css />
+<link rel=stylesheet href=https://<?php echo $HOME_; ?>/input_style.css />
+<link rel=stylesheet href=https://<?php echo $HOME_; ?>/scroll_style.css />
+<link rel=stylesheet href=https://<?php echo $HOME_; ?>/feature/sign/style.css />
+<link rel=stylesheet href=https://<?php echo $HOME_; ?>/feature/search/style.css />
+<link rel=stylesheet href=https://<?php echo $HOME_; ?>/feature/feedback/style.css />
+<link rel=stylesheet href=https://<?php echo $HOME_; ?>/verification_style.css />
+<link rel=stylesheet href=https://<?php echo $HOME_; ?>/services/grocery/grocery_style.css />
+<link rel=stylesheet href=https://<?php echo $HOME_; ?>/services/sell/products/style.css />
+<link rel=stylesheet href=https://<?php echo $HOME_; ?>/services/sell/products/feature/feeling/style.css />
+<link rel=stylesheet href=https://<?php echo $HOME_; ?>/feature/shopping_cart/style.css />
+<link rel="shortcut icon" type="image/png" href="https://<?php echo $HOME_; ?>/resources/logo.png" />
