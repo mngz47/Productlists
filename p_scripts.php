5dbfbd8a6faf02fd7aae505826bc4d88d8a124ad
Mngz <mngz636@gmail.com>
2021-06-22 03:38:24 +0200

    Create p_scripts.php

---
+++ p_scripts.php
@@ -0,0 +1,7 @@
+<script src=https://<?php echo $HOME_; ?>/api.js ></script>
+<script src=https://<?php echo $HOME_; ?>/verification.js ></script>
+<script src=https://<?php echo $HOME_; ?>/feature/search/api.js ></script>
+<script src=https://<?php echo $HOME_; ?>/feature/sign/api.js ></script>
+<script src=https://<?php echo $HOME_; ?>/services/sell/products/api.js ></script>
+<script src=https://<?php echo $HOME_; ?>/feature/shopping_cart/api.js ></script>
+<script src=https://<?php echo $HOME_; ?>/services/sell/products/feature/option/api.js ></script>
