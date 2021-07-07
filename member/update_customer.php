62183e2cf191a627caeaa540b0cfbb2b9de8f818
Mngz <mngz636@gmail.com>
2021-07-07 21:35:45 +0200

    Update update_customer.php

--- member/update_customer.php
+++ member/update_customer.php
@@ -1,7 +1,8 @@
 <?php
-session_start();
+include (str_replace('\\','/',$_SERVER['DOCUMENT_ROOT']).'/config.php');
+
 if(ISSET($_SESSION['customer_id'])){
-$conn = new mysqli('localhost','produc10_mng','mngzpass636','produc10_productlists');
+  
 $sql = 'UPDATE customer SET '.$_GET['fieldname'].'='.str_replace('\"','"',$_GET['fieldval']).' WHERE id='.$_GET['id'].';';
 $result = $conn->query($sql);
 $conn->close();
