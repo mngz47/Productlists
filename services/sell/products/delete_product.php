ae88eac0924ef24b5d049d533f67b71ce4be9e72
Mngz <mngz636@gmail.com>
2021-07-21 03:39:22 +0300

    Update delete_product.php

--- services/sell/products/delete_product.php
+++ services/sell/products/delete_product.php
@@ -1,7 +1,8 @@
 <?php
-session_start();
+
+include (str_replace('\\','/',$_SERVER['DOCUMENT_ROOT']).'/config.php');
+
 if(ISSET($_SESSION['company_id'])){
-$conn = new mysqli('localhost','produc10_mng','mngzpass636','produc10_productlists');
 
 $sql = 'SELECT title,price FROM product WHERE id='.$_GET['product_id'].';';
 $result = $conn->query($sql);
