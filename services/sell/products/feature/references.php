e36e5907d65c616dd464023394352f5d6fb2ee96
Mngz <mngz636@gmail.com>
2021-08-22 21:28:45 +0200

    Update references.php

--- services/sell/products/feature/references.php
+++ services/sell/products/feature/references.php
@@ -8,7 +8,7 @@ $refs = explode('|',$row['refs']);
 for($a=0;$a<count($refs)-1;$a++){
 $p = explode('>',$refs[$a]);
 echo '<a href="'.$p[1].'" target=blank ><div class=block >'.$p[0].
-  (strpos($p[1],"aliexpress")>-1?'<br><img src=https://'.$HOME_.'/resources/AliExpress.png width=100% />':'').
+  (strpos($p[1],"aliexpress")>-1?'<br><img src="https://'.$HOME_.'/resources/AliExpress.png" title="Shop on AliExpress" width=100% />':'').
   '</div></a>';
 }
 }
