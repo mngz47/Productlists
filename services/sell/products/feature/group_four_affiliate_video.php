1fc26c65a3e1a9cdd6dc3763b5cf91ad873d3a94
Mngz <mngz636@gmail.com>
2021-10-22 08:59:36 +0200

    Update group_four_affiliate_video.php

--- services/sell/products/feature/group_four_affiliate_video.php
+++ services/sell/products/feature/group_four_affiliate_video.php
@@ -3,38 +3,20 @@
 <div class=forward >
     <script>
    
-    var affiliates = [
-	    "https://productlists.co.za/services/banners/productlists.html",
-	   "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B06ZY896ZM&asins=B06ZY896ZM&linkId=640f79cc76f9cd0396e8a7a2570b794e&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-	    "https://productlists.co.za/services/banners/kasi_nametest.html",
-	    "https://productlists.co.za/services/banners/recreation.html",
-	    "https://productlists.co.za/services/banners/f_snake.html",
-	    "https://productlists.co.za/services/banners/f_snake.html",
-	    "https://productlists.co.za/services/banners/touchbistro2.html",
-	    "https://productlists.co.za/services/banners/touchbistro2.html",
-	    "https://productlists.co.za/services/banners/cv_engine2.html",
-	    "https://productlists.co.za/services/banners/free_traffic.html",
-	    "https://productlists.co.za/services/banners/jungle_scout2.html",
-	    "https://productlists.co.za/services/banners/adzooma.html",
-	    "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B07K39FRSL&asins=B07K39FRSL&linkId=065aee4d4dac6dbbe3ad4fe7adddc370&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-    "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B08B3X7NXC&asins=B08B3X7NXC&linkId=6d18ac90e6854fe7afdc7f519abc858c&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-    "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B001AWZDA4&asins=B001AWZDA4&linkId=5f4fa3e5c68c638d961e5e69b71d6bc8&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-		     "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B08DLSLCF3&asins=B08DLSLCF3&linkId=46331ad10cb352e3a8e1d6ae437659ce&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-		     "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B07RKPP1YL&asins=B07RKPP1YL&linkId=6073daed0293889abf825e30f1db1235&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-		     "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B07YMM3P8V&asins=B07YMM3P8V&linkId=007c8374941f381f54a96667d8234923&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-		     "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B08BX95GTG&asins=B08BX95GTG&linkId=9442f55d7d283af1894a897f8ef3328b&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-		     "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B07N9KQDVG&asins=B07N9KQDVG&linkId=62b8508dd09e17ce80060a7662d36ca6&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-		     "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B084CVPLLC&asins=B084CVPLLC&linkId=3fd355977db9e7f131df47782e2982ba&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-		     "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B08BX67ZNV&asins=B08BX67ZNV&linkId=6a717631a156a6e3c044477feedba112&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-		     "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B07P6Y8L3F&asins=B07P6Y8L3F&linkId=e6a8a2259adf734cb1cc557382ab7ff9&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-		     "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B08K2JY3Z9&asins=B08K2JY3Z9&linkId=c2a14294c5bbcdcf5492f285cded076e&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-		     "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B008AXYWHQ&asins=B008AXYWHQ&linkId=729e9043d9fe532a604df759ef7a81f0&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-		     "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B002T1M8A8&asins=B002T1M8A8&linkId=4e96703e996ab6902d516cb3620a97d9&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-		     "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B08FCGW4TR&asins=B08FCGW4TR&linkId=f4c8018b2638d65d6b72eb3155033ac3&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-		     "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B0094B79CI&asins=B0094B79CI&linkId=b612979aa6c96c783bc73c62c48803fd&show_border=true&link_opens_in_new_window=true",
-		     "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B085TNBFS9&asins=B085TNBFS9&linkId=1cf98bee56e773fd74f1e748df829aa3&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-		     "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B07H65KP63&asins=B07H65KP63&linkId=cdb6a23b8ff3fc7be7ed5cf491b3b432&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff",
-		     "//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=productlists4-20&marketplace=amazon&region=US&placement=B08923JK7H&asins=B08923JK7H&linkId=c578fa287e4cd061d6232228ec8c8ca8&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff"];
+    var affiliates =
+	[
+	    "https://raw.githubusercontent.com/mngz47/productlists-resources/main/banners/productlists.html",
+	    "https://raw.githubusercontent.com/mngz47/productlists-resources/main/banners/kasi_nametest.html",
+	    "https://raw.githubusercontent.com/mngz47/productlists-resources/main/banners/recreation.html",
+	    "https://raw.githubusercontent.com/mngz47/productlists-resources/main/banners/f_snake.html",
+	    "https://raw.githubusercontent.com/mngz47/productlists-resources/main/banners/f_snake.html",
+	    "https://raw.githubusercontent.com/mngz47/productlists-resources/main/banners/touchbistro2.html",
+	    "https://raw.githubusercontent.com/mngz47/productlists-resources/main/banners/touchbistro2.html",
+	    "https://raw.githubusercontent.com/mngz47/productlists-resources/main/banners/cv_engine2.html",
+	    "https://raw.githubusercontent.com/mngz47/productlists-resources/main/banners/free_traffic.html",
+	    "https://raw.githubusercontent.com/mngz47/productlists-resources/main/banners/jungle_scout2.html",
+	    "https://raw.githubusercontent.com/mngz47/productlists-resources/main/banners/adzooma.html",
+	];
     
     var index = 0;
     
