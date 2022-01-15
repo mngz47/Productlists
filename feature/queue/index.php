8962709c9b4203a68d73790849749b6831a64734
Mngz <mngz636@gmail.com>
2022-01-15 13:06:05 +0200

    Add files via upload

---
+++ feature/queue/index.php
@@ -0,0 +1,69 @@
+
+<p style="font-size:1.1em;" ><strong><a class=fshare href="https://facebook.com/sharer.php?u=https://www.productlists.co.za/services/sell/products/open.php?product_id=<?php echo $_GET['product_id']; ?>" >Refer</a> a friend to lower the price.</strong><br>
+	The price will drop to <span style="color:green;" >R<?php  echo round($row['price']-($row['price']/3)); ?></span><br>
+	After 1000 people have joined the queue.</p>
+<?php
+
+//session_start();
+
+//$conn = new mysqli('localhost','produc10_mng','mngzpass636','produc10_productlists');
+
+//include str_replace('\\','/',$_SERVER['DOCUMENT_ROOT']).'/api.php';
+
+$sql = 'SELECT COUNT(id) AS ii FROM queue WHERE product_id='.$_GET['product_id'];
+$result = $conn->query($sql);
+	       if($q_row = $result->fetch_assoc()){
+			   
+		   if($q_row['ii']>0){
+			   
+                 $q_discount = 	round($row['price'] - (($row['price']/3)*($q_row['ii']/1000)));		   
+			   
+			   echo '<span>Queue Size('.($q_row['ii']+50).')</span><br>';
+			   echo '<span>Product Discount(R'.$q_discount.')</span><br>';
+			   
+			   $msg = '';
+			   
+			   $sql = 'SELECT id,title FROM product WHERE id='.$_GET['product_id'];
+			   $result = $conn->query($sql);
+	       if($result){
+		   if($q_row2 = $result->fetch_assoc()){
+			  
+			   if($q_row['ii']%100==0){
+				   
+				   //message reminder to people in queue (size of queue,price drop)
+				   $msg = 'Queue for <a href="https://www.productlists.co.za/services/sell/products/open.php?product_id='.$q_row2['id'].'" >'.
+				   $q_row2['title'].'</a> now has '.$q_row['ii'].' people and price has dropped to R'.$q_discount.'.<br>'.
+				   ($q_row['ii']%100>7?' Add item to cart and checkout to access discount price':'');
+
+			  } 
+		   }
+		   }
+		   
+		    if($msg){
+				   
+$sql = 'SELECT member_id FROM queue WHERE product_id='.$product_id;
+$result = $conn->query($sql);
+	       if($result){
+			   while($q_row2 = $result->fetch_assoc()){
+				
+           $sql = 'SELECT email_cell FROM customer WHERE id='.$q_row2['member_id'];
+$result2 = $conn->query($sql);
+	       if($result2){
+			   if($q_row3 = $result2->fetch_assoc()){
+				   p_mail($q_row3['email_cell'],$msg);
+			   } 
+		   }	
+			   }   
+			   }
+		   
+			}
+		   }
+		   }
+		   
+		   if(ISSET($_SESSION['customer_id'])){
+				   echo '<a style="font-size:1.2em;" href=# onclick="sendreq(\'feature/queue/newQueue.php?product_id='.$product_id.'\');alert(\'Success\');return false;" >Join Queue</a> - '.
+					   '<a style="font-size:1.2em;" href=# onclick="sendreq(\'feature/queue/exitQueue.php?product_id='.$product_id.'\');alert(\'Success\');return false;" >Exit Queue</a>';
+			   }else{
+				  echo '<p style="font-size:1.2em;" ><a href="https://www.productlists.co.za/member/signin_main.php" >Sign In</a></p>';
+			   }
+?>
---
+++ feature/queue/newQueue.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+
+$conn = new mysqli('localhost','produc10_mng','mngzpass636','produc10_productlists');
+
+if(ISSET($_SESSION['customer_id'])){
+  
+$sql = 'SELECT id FROM queue WHERE member_id='.$_SESSION['customer_id'].' AND product_id='.$_GET['product_id'];
+$result = $conn->query($sql);
+
+  if($row = $result->fetch_assoc()){
+    echo 'Already in queue.';
+  }else{
+    $sql = 'INSERT INTO queue (id,member_id,product_id) VALUES (0,'.$_SESSION['customer_id'].','.$_GET['product_id'].');';
+    $result = $conn->query($sql);
+
+    echo ($result?'Success':'Failure');
+  }
+}else{
+ echo 'Sign In'; 
+}
+
+?>
